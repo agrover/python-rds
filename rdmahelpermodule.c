10e044105a22544078c82ba4968efb1ffffd92d3
Andy Grover <andy.grover@oracle.com>
2010-05-30 23:41:29 -0700

    break get_buffer_info into read and write versions

--- rdmahelpermodule.c
+++ rdmahelpermodule.c
@@ -2,7 +2,22 @@
 #include <Python.h>
 
 static PyObject *
-rdma_get_buffer_info(PyObject *self, PyObject *args)
+rdma_get_read_buffer_info(PyObject *self, PyObject *args)
+{
+	char *ptr;
+	Py_ssize_t len;
+	unsigned long long addr_as_u64;
+
+	if (!PyArg_ParseTuple(args, "s#", &ptr, &len))
+		return NULL;
+
+	addr_as_u64 = (unsigned long)ptr;
+
+	return Py_BuildValue("Kn", addr_as_u64, len);
+}
+
+static PyObject *
+rdma_get_write_buffer_info(PyObject *self, PyObject *args)
 {
 	char *ptr;
 	Py_ssize_t len;
@@ -17,7 +32,9 @@ rdma_get_buffer_info(PyObject *self, PyObject *args)
 }
 
 static PyMethodDef rdma_methods[] = {
-	{"get_buffer_info",  rdma_get_buffer_info, METH_VARARGS,
+	{"get_read_buffer_info",  rdma_get_read_buffer_info, METH_VARARGS,
+	 "Object -> (address, length)"},
+	{"get_write_buffer_info",  rdma_get_write_buffer_info, METH_VARARGS,
 	 "Object -> (address, length)"},
 	 {NULL, NULL, 0, NULL}        /* Sentinel */
 };

c40ad89659fac64aef6d7ac8fe8ff3c8e3c0c538
Andy Grover <andy.grover@oracle.com>
2010-06-01 22:35:15 -0700

    client: rdma received data into a test file

--- client.py
+++ client.py
@@ -8,9 +8,15 @@ sock = rdma.RdmaSocket()
 
 sock.bind("10.1.3.98", 7777)
 
-recv_the_rdma = mmap.mmap(-1, 8192)
+f = open("test.file", "r+w")
+f.write("x")
+f.flush()
 
-cookie = sock.get_mr(recv_the_rdma)
+mapping = mmap.mmap(f.fileno(), 1, prot=mmap.PROT_WRITE, flags=mmap.MAP_PRIVATE)
+
+mapping.resize(8192)
+
+cookie = sock.get_mr(mapping)
 
 cmd = dict(filename="kernel-2.6.18-128.1.16.0.1.el5.x86_64.rpm",
            cookie=cookie,
@@ -20,4 +26,6 @@ cmd = dict(filename="kernel-2.6.18-128.1.16.0.1.el5.x86_64.rpm",
 sock.sendmsg(host="10.1.3.98", port=6666, data=pickle.dumps(cmd))
 
 print sock.recvmsg()
+
+mapping.close()
 
